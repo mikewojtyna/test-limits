a115113a0194f815bc17a5cc4c24e9e3bc78713b
goobar <mike@slidetorial.com>
2018-04-14 21:58:42 +0200

    Add bad MessagePrinter

---
+++ src/main/java/pro/buildmysoftware/testlimits/bad/MessagePrinter.java
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+package pro.buildmysoftware.testlimits.bad;
+
+/**
+ * Let's start with something simple - a class to print message.
+ *
+ * @author goobar
+ *
+ */
+public class MessagePrinter
+{
+	/**
+	 * Prints a message to stdout.
+	 * 
+	 * @param msg
+	 *                a message
+	 */
+	public static void print(String msg)
+	{
+		System.out.println(msg);
+	}
+}
