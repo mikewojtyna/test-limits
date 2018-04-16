2502d380514340f25c6a89ca8fd9ce4752dc815e
goobar <mike@slidetorial.com>
2018-04-16 22:01:11 +0200

    Add bad excelnate "draft test"

---
+++ src/test/java/pro/buildmysoftware/testlimits/bad/excelnate/ExcelReaderTest.java
@@ -0,0 +1,76 @@
+/**
+ *
+ */
+package pro.buildmysoftware.testlimits.bad.excelnate;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Now, I won't even try to start writing tests for that... That would be
+ * totally insane. We would need to test reading files, row mapping and cell
+ * mapping at the same time. Everything coupled together. Also, imagine how to
+ * test exception handling! Just pure madness.
+ *
+ * @author goobar
+ *
+ */
+class ExcelReaderTest
+{
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception
+	{
+	}
+
+	@Test
+	void should_DoAllTheOtherStuff() throws Exception
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void should_HandleCellConversionExceptions() throws Exception
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void should_MapCell() throws Exception
+	{
+		// TODO: create a new test for every cell mapper ever added
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void should_MapComplexObject() throws Exception
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void should_MapRow() throws Exception
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void should_ReadExcelFile() throws Exception
+	{
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception
+	{
+	}
+
+}
