7313ae58cc53710a8ec8a5ebfe5d496be24ac578
goobar <mike@slidetorial.com>
2018-04-16 22:04:39 +0200

    Add more comments why bad Excelnate sample is hard to test

--- src/test/java/pro/buildmysoftware/testlimits/bad/excelnate/ExcelReaderTest.java
+++ src/test/java/pro/buildmysoftware/testlimits/bad/excelnate/ExcelReaderTest.java
@@ -12,7 +12,8 @@ import org.junit.jupiter.api.Test;
  * Now, I won't even try to start writing tests for that... That would be
  * totally insane. We would need to test reading files, row mapping and cell
  * mapping at the same time. Everything coupled together. Also, imagine how to
- * test exception handling! Just pure madness.
+ * test exception handling! And what's worst - we would be forced to test cell
+ * mappers and row mapper through real physical files. Just pure madness.
  *
  * @author goobar
  *
@@ -37,6 +38,8 @@ class ExcelReaderTest
 	@Test
 	void should_HandleCellConversionExceptions() throws Exception
 	{
+		// TODO: the only way to test that would be to place REAL DATA
+		// in physical Excel file... I don't even try to do that here
 		fail("Not yet implemented");
 	}
 
@@ -50,12 +53,16 @@ class ExcelReaderTest
 	@Test
 	void should_MapComplexObject() throws Exception
 	{
+		// TODO: the only way to test that would be to place REAL DATA
+		// in physical Excel file... I don't even try to do that here
 		fail("Not yet implemented");
 	}
 
 	@Test
 	void should_MapRow() throws Exception
 	{
+		// TODO: the only way to test that would be to place REAL DATA
+		// in physical Excel file... I don't even try to do that here
 		fail("Not yet implemented");
 	}
 
