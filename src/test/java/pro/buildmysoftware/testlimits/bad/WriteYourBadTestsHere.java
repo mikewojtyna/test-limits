8fae3d17ec57e92d8a597efd05583f88b78c7d80
goobar <mike@slidetorial.com>
2018-04-16 22:19:43 +0200

    Add empty test placeholders to be used in live coding session

---
+++ src/test/java/pro/buildmysoftware/testlimits/bad/WriteYourBadTestsHere.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package pro.buildmysoftware.testlimits.bad;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author goobar
+ *
+ */
+class WriteYourBadTestsHere
+{
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception
+	{
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/pro/buildmysoftware/testlimits/good/WriteYourGoodTestsHere.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package pro.buildmysoftware.testlimits.good;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author goobar
+ *
+ */
+class WriteYourGoodTestsHere
+{
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception
+	{
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception
+	{
+	}
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
