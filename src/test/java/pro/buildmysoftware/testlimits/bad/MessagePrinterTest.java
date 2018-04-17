002f3d56b4fb622102231a5525ca600bf488d16a
goobar <mike@slidetorial.com>
2018-04-17 21:38:16 +0200

    Remove beforeEach adn afterEach from message printer test

--- src/test/java/pro/buildmysoftware/testlimits/bad/MessagePrinterTest.java
+++ src/test/java/pro/buildmysoftware/testlimits/bad/MessagePrinterTest.java
@@ -6,8 +6,6 @@ package pro.buildmysoftware.testlimits.bad;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -34,22 +32,6 @@ class MessagePrinterTest
 
 		// then
 		assertThat(outputStream.toString()).isEqualTo("hello");
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeEach
-	void setUp() throws Exception
-	{
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterEach
-	void tearDown() throws Exception
-	{
 	}
 
 }
