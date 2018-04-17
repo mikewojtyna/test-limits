635db7535aa47717d92e607aea5e54cd8727443d
goobar <mike@slidetorial.com>
2018-04-18 01:38:08 +0200

    Add bad converter tests

---
+++ src/test/java/pro/buildmysoftware/testlimits/bad/converter/ConverterTest.java
@@ -0,0 +1,106 @@
+/**
+ *
+ */
+package pro.buildmysoftware.testlimits.bad.converter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+import java.util.Optional;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * @author goobar
+ *
+ */
+class ConverterTest
+{
+
+	@Parameters
+	public static Stream<Arguments> params()
+	{
+		return Stream.of(
+
+			// convert bike -> car
+			Arguments.of(bike(), Car.class),
+			// convert bike -> motorcycle
+			Arguments.of(bike(), Motorcycle.class),
+			// convert car -> bike
+			Arguments.of(car(), Bike.class),
+			// convert car -> motorcycle
+			Arguments.of(car(), Motorcycle.class),
+			// convert motorcycle -> car
+			Arguments.of(motorcycle(), Car.class),
+			// convert motorcycle -> bike
+			Arguments.of(motorcycle(), Bike.class)
+
+		);
+	}
+
+	/**
+	 * @return
+	 */
+	private static Object bike()
+	{
+		return new Bike();
+	}
+
+	/**
+	 * @return
+	 */
+	private static Object car()
+	{
+		return new Car();
+	}
+
+	/**
+	 * @return
+	 */
+	private static Object motorcycle()
+	{
+		return new Motorcycle();
+	}
+
+	private Converter converter;
+
+	@DisplayName("should convert supported objects")
+	@ParameterizedTest(name = "source = {0}, target class = {1}")
+	@MethodSource("params")
+	void convert(Object source, Class<?> target) throws Exception
+	{
+		// when
+		Optional<?> result = converter.convert(source, target);
+
+		// then
+		assertThat(result.get()).isInstanceOf(target);
+	}
+
+	@DisplayName("should convert all available types to string")
+	@Test
+	void convertToString() throws Exception
+	{
+		assertThat(converter.convert(bike(), String.class).get())
+			.isEqualTo("Bike");
+		assertThat(converter.convert(car(), String.class).get())
+			.isEqualTo("Car");
+		assertThat(converter.convert(motorcycle(), String.class).get())
+			.isEqualTo("Motorcycle");
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception
+	{
+		converter = new Converter();
+	}
+
+}
