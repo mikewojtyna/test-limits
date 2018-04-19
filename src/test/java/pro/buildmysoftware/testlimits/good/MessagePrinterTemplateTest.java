2458f907d1ab5f186f9c5940a31ba284be28f33a
goobar <mike@slidetorial.com>
2018-04-19 14:31:33 +0200

    Add good message printer template test

---
+++ src/test/java/pro/buildmysoftware/testlimits/good/MessagePrinterTemplateTest.java
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+package pro.buildmysoftware.testlimits.good;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author goobar
+ *
+ */
+class MessagePrinterTemplateTest
+{
+
+	@DisplayName("should print message to stdout")
+	@Test
+	void printStdout() throws Exception
+	{
+		// given
+
+		// when
+
+		// then
+	}
+
+}
