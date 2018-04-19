90498cad6df8cee963794cea220bb5d020db94a2
goobar <mike@slidetorial.com>
2018-04-19 14:38:34 +0200

    Add item price finder happy path template test

---
+++ src/test/java/pro/buildmysoftware/testlimits/ItemPriceFinderHappyPathTemplateTest.java
@@ -0,0 +1,43 @@
+/**
+ *
+ */
+package pro.buildmysoftware.testlimits;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author goobar
+ *
+ */
+class ItemPriceFinderHappyPathTemplateTest
+{
+
+	/*@DisplayName("should return zero as big decimal when item doesn't exist")
+	@Test
+	void testEmpty() throws Exception
+	{
+		// given
+		ItemPriceFinder finder = new ItemPriceFinder();
+
+		// when
+		BigDecimal price = finder.findPrice("Monkey");
+
+		// then
+		assertThat(price).isEqualTo(BigDecimal.valueOf(0));
+	}*/
+
+	@DisplayName("should find price of Rocket launcher")
+	@Test
+	void testFind() throws Exception
+	{
+		// given
+
+		// when
+
+		// then
+		// TODO: Did we forget about something? Uncomment the other
+		// test.
+	}
+
+}
