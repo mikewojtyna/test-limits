0a7e7db5b4ef49d7e743a71d450e0f859ad63c7d
goobar <mike@slidetorial.com>
2018-04-19 14:34:33 +0200

    Add XmasCounter templates

---
+++ src/test/java/pro/buildmysoftware/testlimits/bad/XmasCounterTemplateTest.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+package pro.buildmysoftware.testlimits.bad;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author goobar
+ *
+ */
+class XmasCounterTemplateTest
+{
+
+	@DisplayName("should return number of days until xmas")
+	@Test
+	void daysUntilXmas() throws Exception
+	{
+		// given
+
+		// when
+
+		// then
+	}
+}
---
+++ src/test/java/pro/buildmysoftware/testlimits/good/XmasCounterTemplateTest.java
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+package pro.buildmysoftware.testlimits.good;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author goobar
+ *
+ */
+class XmasCounterTemplateTest
+{
+
+	@DisplayName("should return number of days until xmas")
+	@Test
+	void daysUntilXmas() throws Exception
+	{
+		// given
+
+		// when
+
+		// then
+	}
+
+}
